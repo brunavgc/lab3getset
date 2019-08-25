69654b61b2209f642a59cf99a820272c8e157826
brunavgc <53800964+brunavgc@users.noreply.github.com>
2019-08-25 12:51:59 -0300

    Primeiro Commit Lab3 GetSet

---
+++ src/lab3getset/Exercicio1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3getset;
+
+/**
+ *
+ * @author Negreiros
+ */
+public class Exercicio1 {
+    private String cor;
+    private String marca;
+    private String modelo;
+    private float preco;
+    
+    public String getCor(){
+            return cor;
+    }
+    public void setCor (String pCor){
+        this.cor = pCor;
+    }
+     public String getMarca(){
+            return cor;
+    }
+    public void setMarca (String pMarca){
+        this.cor = pMarca;
+    }
+      public String getModelo(){
+            return modelo;
+    }
+    public void setModelo (String pModelo){
+        this.cor = pModelo;
+    }
+      public float getPreco(){
+            return preco;
+    }
+    public void setPreco (String pPreco){
+        this.cor = pPreco;
+    }
+    
+    
+    
+    
+    
+    
+}
